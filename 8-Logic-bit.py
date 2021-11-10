65c338279cd1cb29dc6b0aff446a5d8a2afa3dfb
Mihai <danfmihai@hotmail.com>
2021-11-10 21:41:21 +0000

    initial commit

---
+++ 8-Logic-bit.py
@@ -0,0 +1,8 @@
+age1 = 24
+age2 = 16
+if (age1 >= 18 and age2 >=18):
+    print("you are both adults")
+elif (age1 >=18 or age2 >= 18):
+    print ("One of you is an adult")
+else:
+    print("You are both children")    
