223215d130ac3c33e709f3a51ae4122a0c3514fd
Mihai <danfmihai@hotmail.com>
2021-11-10 21:11:27 +0000

    edited

--- 4-strings.py
+++ 4-strings.py
@@ -5,3 +5,4 @@ cost_of_apples = 2
 amount_of_apples = input("How many apples do you want? ")
 total = cost_of_apples * int(amount_of_apples)
 print("Total = "+str(total)) 
+print(2 < 4)
---
+++ 5-conditionals.py
@@ -0,0 +1,7 @@
+age = int(input("How old are you? "))
+if age >= 21 :
+    print("You are an adult")
+elif age < 15:
+    print("You are teenager")    
+else:
+    print("You are perfect!")    
