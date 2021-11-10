f4350de479c747d1fccab76e4dea6a028e36da0e
Mihai <danfmihai@hotmail.com>
2021-11-10 21:38:00 +0000

    edited

--- 6-while.py
+++ 6-while.py
@@ -1,6 +1,22 @@
-secret_number = 3
-guess = int(input("Guess a number: "))
-while guess != secret_number:
-    guess = int(input("Guess a number: "))
-else:
-    print("You go it!")    
+# secret_number = 3
+# guess = int(input("Guess a number: "))
+# while guess != secret_number:
+#     guess = int(input("Guess a number: "))
+# else:
+#     print("You go it!")    
+
+# i = 1
+# x = 3
+# sum = 0
+# while ( i <= x ):
+#  sum += i
+#  i += 1
+# print(sum)
+
+
+i = 1
+while True:
+    if i%3 == 0:
+        break
+    print(i)
+    i += 1
---
+++ 7-for.py
@@ -0,0 +1,14 @@
+# for i in range(5):  or for in in range(2,5)
+
+# for i in range(5):
+#     if (i == 2):
+#         break
+#     print("i is: ",i)    
+
+# x = 'abcd'
+# for i in x:
+#     print(i)
+    
+for num in range(0, 11):
+    if num % 2 != 0: print(num)
+print(num)    
