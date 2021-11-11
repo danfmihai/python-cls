34f3549ca76621938a3e76445bc38765464e2356
Mihai <danfmihai@hotmail.com>
2021-11-11 14:09:10 +0000

    added

---
+++ 10-lists.py
@@ -0,0 +1,5 @@
+# elements with index
+country = ["usa","uk","canada"]
+country[0] = "mexico"
+print(country[0])
+print(country[-1])
---
+++ 9-bitwize.py
@@ -0,0 +1,12 @@
+# & - Conjunction 
+# | - Disjuncton
+# ~ - Negation
+# ^ - Exclusive
+
+print(bin(15))
+print(bin(22))
+print(15 & 22)
+# move bits print(22 >> 1) move 1 bit to the right
+#move bits to the left print (22 << 1) 
+print(22 >> 1)
+print (22 << 1)
